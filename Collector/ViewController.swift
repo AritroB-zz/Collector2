f164f064967fbbf829741dd023559055587ac1f5
AritroB <abasuusa2010@gmail.com>
2020-08-13 18:08:20 -0500

    Initial Commit

---
+++ Collector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collector
+//
+//  Created by Aritro Basu on 8/13/20.
+//  Copyright © 2020 Aritro Basu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
